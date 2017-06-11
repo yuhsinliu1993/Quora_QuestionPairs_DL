c2686d325ca4f318de0431d8631fb234511bf1e9
yuhsinliu1993 <abcdegtxx.testing@gmail.com>
2017-06-11 23:56:50 +0800

    add clean_text function

--- utils.py
+++ utils.py
@@ -1,81 +1,76 @@
 import numpy as np
 import re
 import spacy
-from string import punctuation
 
-stop_words = ['the', 'a', 'an', 'and', 'but', 'if', 'or', 'because', 'as', 'what', 'which', 'this', 'that', 'these', 'those', 'then',
-              'just', 'so', 'than', 'such', 'both', 'through', 'about', 'for', 'is', 'of', 'while', 'during', 'to', 'What', 'Which',
-              'Is', 'If', 'While', 'This']
+stopwords = ['the', 'just', 'being', 'through', 'its', 'll', 'had', 'to', 'has', 'this', 'he', 'she', 'each', 'further', 'few', 'doing', 'for', 're', 'be', 'we', 'hers', 'by', 'on', 'about', 'of', 'or', 'own', 'into', 'mightn', 'your', 'from', 'her', 'their', 'there', 'been', 'whom', 'too', 'that', 'with', 'those', 'ma', 'will', 'theirs', 'and', 've', 'then', 'am', 'it', 'an', 'as', 'itself', 'at', 'in', 'any', 'if', 'other', 'you', 'shan', 'such', 'a', 'off', 'i', 'm', 'so', 'y', 'having']
 
 
 def get_cleaned_text(text, remove_stop_words=True):
-    """
-    Stage of preprocessing the dirty texts
-    """
-    # Clean the text
-    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+
+    if remove_stop_words:
+        text = text.lower().split()
+        text = [w for w in text if w not in stopwords]
+        text = " ".join(text)
+
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=_\']", " ", text)
     text = re.sub(r"what's", "", text)
-    text = re.sub(r"What's", "", text)
     text = re.sub(r"\'s", " ", text)
     text = re.sub(r"\'ve", " have ", text)
     text = re.sub(r"can't", "cannot ", text)
     text = re.sub(r"n't", " not ", text)
-    text = re.sub(r"I'm", "I am", text)
-    text = re.sub(r" m ", " am ", text)
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"\bm\b", " ", text)
     text = re.sub(r"\'re", " are ", text)
     text = re.sub(r"\'d", " would ", text)
     text = re.sub(r"\'ll", " will ", text)
-    text = re.sub(r"60k", " 60000 ", text)
-    text = re.sub(r" e g ", " eg ", text)
-    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r"\be g\b", " eg ", text)
+    text = re.sub(r"\bb g\b", " bg ", text)
     text = re.sub(r"\0s", "0", text)
-    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"\b9 11\b", "911", text)
     text = re.sub(r"e-mail", "email", text)
-    text = re.sub(r"\s{2,}", " ", text)
+    text = re.sub(r"\bidk\b", "i do not know ", text)
     text = re.sub(r"quikly", "quickly", text)
-    text = re.sub(r" usa ", " America ", text)
-    text = re.sub(r" USA ", " America ", text)
-    text = re.sub(r" u s ", " America ", text)
-    text = re.sub(r" uk ", " England ", text)
-    text = re.sub(r" UK ", " England ", text)
-    text = re.sub(r"india", "India", text)
-    text = re.sub(r"switzerland", "Switzerland", text)
-    text = re.sub(r"china", "China", text)
-    text = re.sub(r"chinese", "Chinese", text)
+    text = re.sub(r"\bomg\b", " oh my god ", text)
+    text = re.sub(r"\busa\b", " america ", text)
+    text = re.sub(r"\bUSA\b", " america ", text)
+    text = re.sub(r"\bu s\b", " america ", text)
+    text = re.sub(r"\buk\b", " england ", text)
+    text = re.sub(r"\bUK\b", " england ", text)
     text = re.sub(r"imrovement", "improvement", text)
     text = re.sub(r"intially", "initially", text)
-    text = re.sub(r"quora", "Quora", text)
-    text = re.sub(r" dms ", "direct messages ", text)
+    text = re.sub(r"\bdms\b", "direct messages ", text)
     text = re.sub(r"demonitization", "demonetization", text)
     text = re.sub(r"actived", "active", text)
     text = re.sub(r"kms", " kilometers ", text)
     text = re.sub(r"KMs", " kilometers ", text)
-    text = re.sub(r" cs ", " computer science ", text)
-    text = re.sub(r" upvotes ", " up votes ", text)
-    text = re.sub(r" iPhone ", " phone ", text)
+    text = re.sub(r"\bcs\b", " computer science ", text)
+    text = re.sub(r"\bupvotes\b", " up votes ", text)
+    text = re.sub(r"\biPhone\b", " phone ", text)
     text = re.sub(r"\0rs ", " rs ", text)
     text = re.sub(r"calender", "calendar", text)
     text = re.sub(r"ios", "operating system", text)
-    text = re.sub(r"gps", "GPS", text)
-    text = re.sub(r"gst", "GST", text)
     text = re.sub(r"programing", "programming", text)
     text = re.sub(r"bestfriend", "best friend", text)
-    text = re.sub(r"dna", "DNA", text)
     text = re.sub(r"III", "3", text)
-    text = re.sub(r"the US", "America", text)
-    text = re.sub(r"Astrology", "astrology", text)
-    text = re.sub(r"Method", "method", text)
-    text = re.sub(r"Find", "find", text)
-    text = re.sub(r"banglore", "Banglore", text)
-    text = re.sub(r" J K ", " JK ", text)
+    text = re.sub(r"the us", "america", text)
+    text = re.sub(r"\bj k\b", " jk ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ", text)
+    text = re.sub(r"\+", " ", text)
+    text = re.sub(r"\-", " ", text)
+    text = re.sub(r"\=", " ", text)
+    text = re.sub(r"_", " ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r":", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r"\s{2,}", " ", text)
 
-    # Remove punctuation from text
-    text = ''.join([c for c in text if c not in punctuation])
-
-    # Optionally, remove stop words
     if remove_stop_words:
         text = text.split()
-        text = [w for w in text if w not in stop_words]
+        text = [w for w in text if w not in stopwords]
         text = " ".join(text)
 
     return text
